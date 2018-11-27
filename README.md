51f4e6f1d9040039b1df5f1802820a7f967e74a2
Christophe Gagnier <c.gagnier11@gmail.com>
2018-11-26 22:25:58 -0500

    Started Menu. Started Race Map. Added pause option

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Game Off 2018 Entry
 
 # Music
 Spinnin' by Alex (c) copyright 2011 Licensed under a Creative Commons Attribution (3.0) license. http://dig.ccmixter.org/files/AlexBeroza/32423 Ft: spinningmerkaba
+
+# Font
+Copyright (c) 2012, Fernando Haro <fernando@defharo.com> & deFharo.com,
+with Reserved Font Name "Hobby of night". Licensed under a SIL Open Font License Version 1.1
+
--- Screaming Tires/scripts/GameController.cs
+++ Screaming Tires/scripts/GameController.cs
@@ -4,18 +4,43 @@ using System;
 public class GameController : Node
 {
 
-    Vector3 START_POSITION = new Vector3();
+    //Vector3 START_POSITION = new Vector3();
+
+    Container pauseScreen;
+
     public override void _Ready()
     {
         // Should read from loaded map what is the starting position for the player and place the player
+        // Place the checkpoints
+        // Start a Timer
+        // 
+        pauseScreen = (Container) GetNode("UI/HBox/PausedScreen");
+        pauseScreen.Visible = false;
         
     }
 
     public override void _Process(float delta)
     {
         if (Input.IsActionJustPressed("ui_accept")) {
-            // Reset car to starting position;
+            // Reset car to starting position then add 1 sec to the timer
+
         }
-        
+
+
+
+
+        if (Input.IsActionJustPressed("ui_cancel")) 
+            TogglePause(true);
+           
+    }
+    // Use to toggle the game pause state
+    public void TogglePause(bool state) {
+        GD.Print("Toggle Pause " + state.ToString());
+
+        pauseScreen.Visible = state;
+
+        GetTree().Paused = state;
+
+
     }
 }
